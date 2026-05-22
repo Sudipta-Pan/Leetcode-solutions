2a8b05646c01895f250d5cf066f3ed70df71d4a3
Sudipta Pan <sudiptapan54@gmail.com>
2026-05-23 00:15:11 +0530

    Time: 6 ms (8.78%) | Memory: 44.5 MB (94.57%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,35 +1,29 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        // Map to store: element -> its next greater element
-        HashMap<Integer, Integer> map = new HashMap<>();
-        Stack<Integer> stack = new Stack<>();
+        int[] arr = new int[nums1.length];
+        int x = 0;
+        boolean bool = true;
+
+        for(int i = 0; i < nums1.length; i++) {
         
-        // Process nums2 from right to left
-        for (int i = nums2.length - 1; i >= 0; i--) {
-            int num = nums2[i];
-            
-            // Pop elements from the stack that are smaller than or equal to the current number
-            while (!stack.isEmpty() && stack.peek() <= num) {
-                stack.pop();
+            for(int j = 0; j<nums2.length; j++) {
+        
+                if(nums1[i] == nums2[j]){
+                    bool = true;
+        
+                    for(int k = j+1; k < nums2.length; k++){
+        
+                        if(nums2[k] > nums2[j]){
+                            arr[x++] = nums2[k];
+                            bool = false;
+                            break;
+                        }
+                    }
+                    if(bool)
+                        arr[x++] = -1;
+                }
             }
-            
-            // If stack is empty, there is no greater element to the right. Otherwise, it's the top element.
-            if (stack.isEmpty()) {
-                map.put(num, -1);
-            } else {
-                map.put(num, stack.peek());
-            }
-            
-            // Push the current number onto the stack for future elements to check
-            stack.push(num);
         }
-        
-        // Build the result array for nums1 using our map lookups
-        int[] result = new int[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
-            result[i] = map.get(nums1[i]);
-        }
-        
-        return result;
+        return arr;
     }
 }
